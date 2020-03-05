c5500035056305468d715419e022e56f6756aa90
vanhock <vanhocker@gmail.com>
2020-03-05 17:57:30 +0300

    Select collection template

---
+++ collectionTemplates.js
@@ -0,0 +1,21 @@
+/**
+ * Список тем для которых есть шаблоны collection.liquid
+ * @type {*[]}
+ */
+module.exports = [
+  {
+    name: "Стандартный",
+    folder: "default",
+    image: "default.jpg"
+  },
+  {
+    name: "Fleur",
+    folder: "fleur",
+    image: "fleur.jpg"
+  },
+  {
+    name: "Аромат 2.0",
+    folder: "aromat",
+    image: "aromat.png"
+  },
+];
